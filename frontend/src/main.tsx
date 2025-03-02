7ccbdc18b216a04c1b8c9e9b38d23eba44d9fbf9
ywow-ai <masyaqin37@gmail.com>
2025-03-02 19:18:30 +0700

    feat: perbaikan navbar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/main.css";
+// import App from "./App";
 // import "./assets/bs-to-tw.css";
 import Routes from "./Routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Routes />
+    {/* <App /> */}
   </StrictMode>
 );
