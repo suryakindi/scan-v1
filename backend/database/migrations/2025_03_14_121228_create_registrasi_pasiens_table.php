1fe00b9bfe5215fe58cff59ecea99b7fc53b41a4
Surya Kindi Bayhaqi <127248092+suryakindi@users.noreply.github.com>
2025-04-14 20:13:28 +0700

    update status pulang

--- backend/database/migrations/2025_03_14_121228_create_registrasi_pasiens_table.php
+++ backend/database/migrations/2025_03_14_121228_create_registrasi_pasiens_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
             $table->boolean('is_active')->default(1);
             $table->unsignedBigInteger('cdfix');
             $table->unsignedBigInteger('status_pasien');
+            $table->unsignedBigInteger('status_pulang')->nullable();
             $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
             $table->unsignedBigInteger('deleted_by')->nullable();
